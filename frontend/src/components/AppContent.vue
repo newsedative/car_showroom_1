7da4f1a9e48e00af70e47037c285d2fd3bef47a8
Dsh <knowslana@gmail.com>
2024-07-19 02:12:01 +0300

    модальное окно теперь работает

--- frontend/src/components/AppContent.vue
+++ frontend/src/components/AppContent.vue
@@ -2,34 +2,39 @@
 import CreateForm from "@/components/CreateForm.vue";
 import CarsList from "@/components/CarsList.vue";
 import MDialog from "@/components/UI/MDialog.vue";
+import MButton from "@/components/UI/MButton.vue";
 
 export default {
   name: "AppContent",
-  components: {MDialog, CreateForm, CarsList},
+  components: {MButton, MDialog, CreateForm, CarsList},
   data() {
     return {
       cars: [
         {id: 1, brand: "Mazda", model: '6 III GJ', mileage: 35000, year: 2023},
       ],
+      dlgShowOrNot: false,
     }
   },
   methods: {
     createCar(data) {
       console.log(data)
       this.cars.push(data)
+    },
+    dlgShow() {
+      this.dlgShowOrNot = true
     }
-  }
+  },
 }
 </script>
 
 <template>
   <div>
     <div class="x-content">
-      <m-dialog :show="true">
+      <m-button @click="dlgShow" style="padding: 2%; align-self: flex-end">Добавить</m-button>
+      <m-dialog v-model:show="dlgShowOrNot">
         <create-form @create="createCar"></create-form>
       </m-dialog>
-      <create-form @create="createCar"></create-form>
-      <cars-list :cars="cars" @remove="deleteCar"></cars-list>
+      <cars-list :cars="cars"></cars-list>
     </div>
   </div>
 
--- frontend/src/components/UI/MDialog.vue
+++ frontend/src/components/UI/MDialog.vue
@@ -17,6 +17,7 @@ export default {
   },
   methods: {
     hideDialog() {
+      this.$emit('update:show', false)
     }
   }
 }
