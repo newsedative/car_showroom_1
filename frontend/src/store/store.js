d6792ba5b82fb9ff6f606cb270acdd56343872db
Dsh <knowslana@gmail.com>
2024-07-23 18:08:30 +0300

    что-то

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -5,55 +5,32 @@ export const store = {
     namespaced: true,
     state: () => ({
         token: localStorage.getItem('token')||"",
-        username: localStorage.getItem('username')||"",
-        password: localStorage.getItem('password')||"",
     }),
     getters: {
         isLoggedIn: state => !!state.token
     },
     mutations: {
-        setToken(state, token, username, password) {
+        setToken(state, token) {
             state.token = token
             localStorage.setItem('token', token)
-            localStorage.setItem('username', username)
-            localStorage.setItem('password', password)
         },
         removeToken(state) {
             state.token = ""
-            state.username = ""
-            state.password = ""
             localStorage.setItem('token', "")
-            localStorage.setItem('username', null)
-            localStorage.setItem('password', null)
         }
     },
     actions: {
-        async login({commit}, {username, password, email, router}) {
+        async login({commit}, {username, password, router}) {
             try {
                 const response = await axios.post("https://newsedative.pythonanywhere.com/api-token-auth/", {
                   username: username,
-                  password: password,
-                  email: email,
+                  password: password
                 })
                 const token = response.data.token
                 await commit("setToken", token, username, password)
                 router.push("/user")
             } catch (error) {
                 alert("Пароль или логин неверны!")
-            }
-        },
-        async register({commit}, {username, password, email, router}) {
-            try {
-                const response = await axios.post("http://localhost:8000/account/register/", {
-                  username: username,
-                  password: password,
-                  email: email
-                })
-                const token = response.data.token
-                await commit("setToken", token)
-                router.push("/login")
-            } catch (error) {
-                alert("Пользователь с таким логином уже существует!")
             }
         }
     },
