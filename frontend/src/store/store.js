372c4b488df03250c9a9193c26520ae7fe1a2160
Dsh <knowslana@gmail.com>
2024-07-25 07:54:32 +0300

    изменения

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -30,7 +30,7 @@ export const store = {
     actions: {
         async login({commit}, {username, password, router}) {
             try {
-                const response = await axios.post("http://127.0.0.1:8000/api-token-auth/", {
+                const response = await axios.post("http://51.250.107.34/api-token-auth/", {
                   'username': username,
                   'password': password,
                 })
