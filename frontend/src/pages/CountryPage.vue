ffb83f52b833c9b5dd8f8115fac95f5d49a2c930
Dsh <knowslana@gmail.com>
2024-07-25 07:35:31 +0300

    добавление vuetify

--- frontend/src/pages/CountryPage.vue
+++ frontend/src/pages/CountryPage.vue
@@ -10,177 +10,209 @@ export default {
   data() {
     return {
       countries: [],
-      dlgShowOrNot: false,
-      isCountryLoading: false,
       headers: [
         {text: 'ID', value: 'id'},
         {text: 'Название', value: 'country_name'},
         {text: 'Код', value: 'code', sortable: false,},
         {text: 'Валюта', value: 'currency', sortable: false,},
-        {text: 'Действие', value: 'action', sortable: false,},
+        {text: 'Действие', value: 'actions', sortable: false,},
       ],
       search: '',
+      dialog: false,
+      dialogDelete: false,
+      desserts: [],
+      editedIndex: -1,
+      country: {
+        country_name: '',
+        code: '',
+        currency: '',
+      },
     }
   },
-  mounted() {
-    this.fetchCountry();
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
   },
+
+  created() {
+    this.initialize()
+  },
+
   methods: {
-    createCountry(data) {
+    createCountry() {
+      let country = {...this.country}
       const content = {
-        country_name: data.country_name,
-        code: data.code,
-        currency: data.currency
+        country_name: country.country_name,
+        code: country.code,
+        currency: country.currency
       }
       this.$ajax.post('api/country/', content)
-          .then(response => this.countries.push({...data, id: response.data.id}))
-      this.dlgShowOrNot = false
+                .then(response => this.countries.push({...country, id:response.data.id}))
+      this.close()
     },
-    removeCountry(country) {
-      this.$ajax.delete(`api/country/${country.id}/`)
-      this.countries = this.countries.filter(elem => elem.id !== country.id)
+    deleteItem(item) {
+      this.$ajax.delete(`api/country/${item.id}/`)
+      this.countries = this.countries.filter(elem => elem.id !== item.id)
     },
-    async fetchCountry() {
-      try {
-        this.isCountryLoading = true;
-        const response = await this.$ajax.get('api/country/');
-        console.log(response);
-        this.countries = response.data;
-      } catch (e) {
-        alert('error');
-      } finally {
-        // eslint-disable-next-line no-debugger
-        debugger;
-        this.isCountryLoading = false;
-      }
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedIndex = -1
+      })
     },
     logout() {
       this.$store.commit('login/removeToken')
       this.$router.push('/login')
     },
-    showDialog() {
-      this.dlgShowOrNot = true;
+    async fetchCountry() {
+       try {
+        const response = await this.$ajax.get('api/country/');
+        console.log(response);
+        this.countries = response.data;
+      } catch (e) {
+        alert('error');
+      }
     }
+  },
+  mounted() {
+    this.fetchCountry();
   },
 }
 </script>
 
 <template>
-  <v-container>
-    <v-toolbar flat>
-      <v-toolbar-title>Машинова Дарья</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <div>
-        <v-btn color="#D05353" @click="logout">Выход</v-btn>
-      </div>
-    </v-toolbar>
-    <v-app-bar
-        elevation="4"
-        color="#D05353"
-    >
-      <v-tabs align-with-title>
-        <v-tab @click="$router.push('/country')">Страны</v-tab>
-        <v-tab @click="$router.push('/cars')">Автомобили</v-tab>
-        <v-tab @click="$router.push('/carpart')">Запчасти</v-tab>
-      </v-tabs>
-
-    </v-app-bar>
-
-    <div>
-      <v-text-field
-          v-model="search"
-          label="Поиск..."
-          :rules="rules"
-          hide-details="auto"
-      ></v-text-field>
-    </div>
-
-    <v-toolbar
-        flat
-    >
-      <v-btn
+  <v-app>
+    <v-container>
+      <v-toolbar flat>
+        <v-toolbar-title>Машинова Дарья</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <div>
+          <v-btn color="#D05353" @click="logout">Выход</v-btn>
+        </div>
+      </v-toolbar>
+      <v-app-bar
+          elevation="4"
           color="#D05353"
-          class="mb-2"
-          @click="showDialog"
       >
-        Добавить
-      </v-btn>
-      <v-dialog
-          v-model="dlgShowOrNot"
-          width="500"
+        <v-tabs align-with-title>
+          <v-tab @click="$router.push('/country')">Страны</v-tab>
+          <v-tab @click="$router.push('/cars')">Автомобили</v-tab>
+          <v-tab @click="$router.push('/carpart')">Запчасти</v-tab>
+        </v-tabs>
+
+      </v-app-bar>
+
+      <div>
+        <v-text-field
+            v-model="search"
+            label="Поиск..."
+            :rules="rules"
+            hide-details="auto"
+        ></v-text-field>
+      </div>
+
+      <v-data-table
+          :headers="headers"
+          :items="countries"
+          class="elevation-1"
+          :search="search"
       >
-        <v-card>
-          <v-card-title>
-            <span class="text-h5">Добавить страну</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                >
-                  <v-text-field
-                      label="Название"
-                      required
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                >
-                  <v-text-field
-                      label="Код"
-                  ></v-text-field>
-                </v-col>
-                <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                >
-                  <v-text-field
-                      label="Валюта"
-                      required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-                color="blue darken-1"
-                text
-                @click="dlgShowOrNot = false"
+        <template v-slot:top>
+          <v-toolbar
+              flat
+          >
+            <v-dialog
+                v-model="dialog"
+                max-width="500px"
             >
-              Close
-            </v-btn>
-            <v-btn
-                color="blue darken-1"
-                text
-                @click="dlgShowOrNot = false"
-            >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                  Добавить
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                      >
+                        <v-text-field
+                            v-model="country.country_name"
+                            label="Название"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                      >
+                        <v-text-field
+                            v-model="country.code"
+                            label="Код"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                      >
+                        <v-text-field
+                            v-model="country.currency"
+                            label="Валюта"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
 
-    </v-toolbar>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="close"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="createCountry"
+                  >
+                    Сохранить
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-icon
+              small
+              @click="deleteItem(item)"
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+      </v-data-table>
 
-    <v-data-table
-        :headers="headers"
-        :items="countries"
-        class="elevation-1"
-        :search="search"
-    >
-    </v-data-table>
 
-
-  </v-container>
+    </v-container>
+  </v-app>
 </template>
+
 
 <style scoped>
 </style>
